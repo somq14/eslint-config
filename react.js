6640e52b58aa23bf20907281d4a63892464746ed
somq14 <som.q14@gmail.com>
2022-02-13 12:07:17 +0900

    tmp

--- react.js
+++ react.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     "./index.js",
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "prettier",
